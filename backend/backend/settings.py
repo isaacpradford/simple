89b9dbe68d0061b2e53bc597c0c3feb4891c1cf0
isaacpradford <radf.isaac@gmail.com>
2025-05-21 18:42:18 -0500

    bug fixes for deploying

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "backend.app",
+    "app",
     "rest_framework",
     "corsheaders",
     "django_extensions",
